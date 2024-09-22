762a10cc46cd3d7b0672b6a76f3dedf3a49db5b4
kristof1345 <kristofnemeth1345@gmail.com>
2024-09-22 23:14:53 +0200

    adding shortcuts

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -11,13 +12,17 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Go Editor")
 
-	w.Resize(fyne.NewSize(600, 400))
+	ctrlO := &desktop.CustomShortcut{KeyName: fyne.KeyO, Modifier: fyne.KeyModifierControl}
+	w.Canvas().AddShortcut(ctrlO, func(shortcut fyne.Shortcut) {
+		w.SetContent(widget.NewLabel("Shit, this works???"))
+	})
 
 	label := widget.NewLabel("Press CTRL+O to open a file")
 	content := container.NewCenter(label)
 
 	// editor := widget.NewMultiLineEntry()
 
+	w.Resize(fyne.NewSize(600, 400))
 	w.SetContent(content)
 
 	w.ShowAndRun()
