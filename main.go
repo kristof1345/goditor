b923c3bcb6c9afe94290591704791d25bfbf6117
kristof1345 <kristofnemeth1345@gmail.com>
2024-09-23 08:18:41 +0200

    some structs

--- main.go
+++ main.go
@@ -8,12 +8,43 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+type EditorApp struct {
+	Tabs      []Tab
+	ActiveTab string
+}
+
+type CursorPosition struct {
+	x int
+	y int
+}
+
+type Tab struct {
+	FilePath       string
+	CursorPosition CursorPosition
+	Contents       string
+	ID             string
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
 	a := app.New()
 	w := a.NewWindow("Go Editor")
+	// var editor EditorApp
 
+	// shouldn't I put this into a separate go routine?
 	ctrlO := &desktop.CustomShortcut{KeyName: fyne.KeyO, Modifier: fyne.KeyModifierControl}
 	w.Canvas().AddShortcut(ctrlO, func(shortcut fyne.Shortcut) {
+		// I should definitely put this into a go routing, at least the file open
+		// open dialog
+		// get file path
+		// read file, if that succeeds:
+		// create a tab -> add it to EditorApp
+		// render
 		w.SetContent(widget.NewLabel("Shit, this works???"))
 	})
 
