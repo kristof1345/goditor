1843ca839c9716dad0fafda186c0d1a5e75fc103
kristof1345 <kristofnemeth1345@gmail.com>
2024-09-24 16:26:40 +0200

    quit message and nicer key processing

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/kristof1345/goditor
 
 go 1.23.1
 
-require (
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/term v0.24.0 // indirect
-)
+require golang.org/x/term v0.24.0
+
+require golang.org/x/sys v0.25.0 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"golang.org/x/term"
 	"os"
-	"unicode"
+	// "unicode"
 )
 
 func CONTROL_KEY(key byte) int {
@@ -27,11 +27,36 @@ func enableRawMode() {
 	}
 }
 
+func editorReadKey() byte {
+	b := make([]byte, 1)
+	_, err := os.Stdin.Read(b)
+
+	if err != nil {
+		die("processing key")
+	}
+
+	return b[0]
+}
+
+func editorProcessKeyPress() {
+	ch := editorReadKey()
+
+	switch ch {
+	case byte(CONTROL_KEY('q')):
+		exit("CTRL+Q was pressed")
+	}
+}
+
 func die(str string) {
 	term.Restore(int(os.Stdout.Fd()), terminalState)
-
 	fmt.Println(str)
 	os.Exit(1)
+}
+
+func exit(str string) {
+	term.Restore(int(os.Stdout.Fd()), terminalState)
+	fmt.Println(str)
+	os.Exit(0)
 }
 
 /*** init ***/
@@ -39,16 +64,17 @@ func die(str string) {
 func main() {
 	enableRawMode()
 
-	b := make([]byte, 1)
+	// b := make([]byte, 1)
 	for {
-		os.Stdin.Read(b)
-		if b[0] == byte(CONTROL_KEY('q')) {
-			die("CTRL+Q was clicked")
-		}
-		if unicode.IsControl(rune(b[0])) {
-			fmt.Printf("%d\r\n", b[0])
-		} else {
-			fmt.Printf("%d, ('%c')\r\n", b[0], b[0])
-		}
+		// os.Stdin.Read(b)
+		// if b[0] == byte(CONTROL_KEY('q')) {
+		// 	die("CTRL+Q was clicked")
+		// }
+		// if unicode.IsControl(rune(b[0])) {
+		// 	fmt.Printf("%d\r\n", b[0])
+		// } else {
+		// 	fmt.Printf("%d, ('%c')\r\n", b[0], b[0])
+		// }
+		editorProcessKeyPress()
 	}
 }
