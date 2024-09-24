9da0aeaec5f2ba799d4558c83976b3190abd6103
kristof1345 <kristofnemeth1345@gmail.com>
2024-09-24 10:25:00 +0200

    rework: wasn't able to make GUI version work; now we are building a terminal code editor(Note: Go is better in the terminal anyways)

--- main.go
+++ main.go
@@ -1,87 +1,38 @@
 package main
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/driver/desktop"
-	"fyne.io/fyne/v2/widget"
-	"io"
+	"fmt"
+	"golang.org/x/term"
+	"os"
 )
 
-type EditorApp struct {
-	Tabs      []Tab
-	ActiveTab string
-}
+var terminalState *term.State
 
-type CursorPosition struct {
-	x int
-	y int
-}
+func enableRawMode() {
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	terminalState = oldState
 
-type Tab struct {
-	FilePath       string
-	CursorPosition CursorPosition
-	Contents       string
-	ID             string
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
+	if err != nil {
+		// restore and kill
+		die(err.Error())
 	}
+}
+
+func die(str string) {
+	term.Restore(int(os.Stdout.Fd()), terminalState)
+
+	fmt.Println(str)
+	os.Exit(1)
 }
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Go Editor")
-	// var editor EditorApp
+	enableRawMode()
 
-	fileContent := widget.NewMultiLineEntry()
-
-	openFileDialog := func() {
-		dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
-			if err != nil {
-				dialog.ShowError(err, w)
-				return
-			}
-			if reader == nil {
-				// user quit
-				return
-			}
-
-			data, err := io.ReadAll(reader)
-			if err != nil {
-				dialog.ShowError(err, w)
-				return
-			}
-
-			fileContent.SetText(string(data))
-			w.SetContent(fileContent)
-		}, w).Show()
+	b := make([]byte, 1)
+	for {
+		os.Stdin.Read(b)
+		if b[0] == 'q' {
+			die("q was clicked")
+		}
 	}
-
-	// shouldn't I put this into a separate go routine?
-	ctrlO := &desktop.CustomShortcut{KeyName: fyne.KeyO, Modifier: fyne.KeyModifierControl}
-	w.Canvas().AddShortcut(ctrlO, func(shortcut fyne.Shortcut) {
-		// I should definitely put this into a go routing, at least the file open
-		// open dialog
-		openFileDialog()
-		// get file path
-		// read file, if that succeeds:
-		// create a tab -> add it to EditorApp
-		// render
-		w.SetContent(widget.NewLabel("Shit, this works???"))
-	})
-
-	label := widget.NewLabel("Press CTRL+O to open a file")
-	content := container.NewCenter(label)
-
-	// editor := widget.NewMultiLineEntry()
-
-	w.Resize(fyne.NewSize(600, 400))
-	w.SetContent(content)
-
-	w.ShowAndRun()
 }
