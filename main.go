1e74ff830053b6ee1ca8ec0b3daa096199d215de
kristof1345 <kristofnemeth1345@gmail.com>
2024-09-25 14:33:53 +0200

    cursor is properly moving

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	// "unicode"
 )
 
+var GODITOR_VERSION = "0.0.1"
+
 func CONTROL_KEY(key byte) int {
 	return int(key & 0x1f)
 }
@@ -15,12 +17,20 @@ func CONTROL_KEY(key byte) int {
 /*** data ***/
 
 type EditorConfig struct {
+	cursor_x, cursor_y     int
 	screenrows, screencols int
 }
 
 var terminalState *term.State
 var byteBuffer = bytes.Buffer{}
 var editorConfig = EditorConfig{}
+
+const (
+	ARROW_LEFT = iota + 1000
+	ARROW_RIGHT
+	ARROW_UP
+	ARROW_DOWN
+)
 
 /*** terminal ***/
 
@@ -34,19 +44,59 @@ func enableRawMode() {
 	}
 }
 
-func editorReadKey() byte {
-	b := make([]byte, 1)
+func editorReadKey() int {
+	b := make([]byte, 4)
 	_, err := os.Stdin.Read(b)
 
 	if err != nil {
 		die("processing key")
 	}
 
-	return b[0]
+	if b[0] == '\x1b' {
+		if b[1] == '[' {
+			switch b[2] {
+			case 'A':
+				return ARROW_UP
+			case 'B':
+				return ARROW_DOWN
+			case 'C':
+				return ARROW_RIGHT
+			case 'D':
+				return ARROW_LEFT
+			}
+		}
+
+		return '\x1b'
+	} else {
+		return int(b[0])
+	}
+
+	// switch c {
+	// case 1:
+	// 	return int(b[0])
+	// case 2:
+	// 	return '\x1b'
+	// case 3, 4:
+	// 	if b[0] == '\x1b' {
+	// 		if b[1] == '[' {
+	// 			switch b[2] {
+	// 			case 'A':
+	// 				return ARROW_UP
+	// 			case 'B':
+	// 				return ARROW_LEFT
+	// 			case 'C':
+	// 				return ARROW_RIGHT
+	// 			case 'D':
+	// 				return ARROW_DOWN
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
 
 func die(str string) {
 	os.Stdout.Write([]byte("\x1b[2J"))
+	os.Stdout.Write([]byte("\x1b[3J"))
 	os.Stdout.Write([]byte("\x1b[H"))
 	term.Restore(int(os.Stdout.Fd()), terminalState)
 	fmt.Println(str)
@@ -58,12 +108,35 @@ func die(str string) {
 func editorProcessKeyPress() {
 	ch := editorReadKey()
 	switch ch {
-	case byte(CONTROL_KEY('q')):
-		os.Stdout.Write([]byte("\x1b[2J"))
-		os.Stdout.Write([]byte("\x1b[3J"))
+	case CONTROL_KEY('q'):
+		os.Stdout.Write([]byte("\x1b[2J")) // clear screen
+		os.Stdout.Write([]byte("\x1b[3J")) // clear scrollback history
 		os.Stdout.Write([]byte("\x1b[H"))
 		term.Restore(int(os.Stdout.Fd()), terminalState)
 		os.Exit(0)
+	case ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT, ARROW_UP:
+		editorMoveCursor(ch)
+	}
+}
+
+func editorMoveCursor(key int) {
+	switch key {
+	case ARROW_LEFT:
+		if editorConfig.cursor_x != 0 {
+			editorConfig.cursor_x--
+		}
+	case ARROW_RIGHT:
+		if editorConfig.cursor_x != editorConfig.screencols-1 {
+			editorConfig.cursor_x++
+		}
+	case ARROW_UP:
+		if editorConfig.cursor_y != 0 {
+			editorConfig.cursor_y--
+		}
+	case ARROW_DOWN:
+		if editorConfig.cursor_y != editorConfig.screenrows-1 {
+			editorConfig.cursor_y++
+		}
 	}
 }
 
@@ -71,7 +144,23 @@ func editorProcessKeyPress() {
 
 func editorDrawRows() {
 	for i := 0; i < editorConfig.screenrows; i++ {
-		byteBuffer.WriteString("~")
+		if i == editorConfig.screenrows/3 {
+			welcome := fmt.Sprintf("Goditor editor -- version: %s", GODITOR_VERSION)
+
+			padding := (editorConfig.screencols - len(welcome)) / 2
+			if padding > 0 {
+				byteBuffer.WriteString("~")
+			}
+			for ; padding > 0; padding-- {
+				byteBuffer.WriteString(" ")
+			}
+
+			byteBuffer.WriteString(welcome)
+		} else {
+			byteBuffer.WriteString("~")
+		}
+
+		byteBuffer.WriteString("\x1b[K") // clear reset of the line
 		if i < editorConfig.screenrows-1 {
 			byteBuffer.WriteString("\r\n")
 		}
@@ -79,16 +168,21 @@ func editorDrawRows() {
 }
 
 func editorRefreshScreen() {
-	byteBuffer.WriteString("\x1b[2J")
-	byteBuffer.WriteString("\x1b[3J")
+	byteBuffer.WriteString("\x1b[?25l") // hide cursor
 	byteBuffer.WriteString("\x1b[H")
-	// os.Stdout.Write([]byte("\x1b[2J"))
-	// os.Stdout.Write([]byte("\x1b[H"))
 
 	editorDrawRows()
 
-	byteBuffer.WriteString("\x1b[H")
-	// os.Stdout.Write([]byte("\x1b[H"))
+	// byteBuffer.WriteString("\x1b[H")
+	byteBuffer.WriteString(
+		fmt.Sprintf(
+			"\x1b[%d;%dH",
+			(editorConfig.cursor_y + 1),
+			(editorConfig.cursor_x + 1),
+		),
+	)
+
+	byteBuffer.WriteString("\x1b[?25h") // reposition cursor
 
 	os.Stdout.Write(byteBuffer.Bytes())
 }
@@ -104,6 +198,8 @@ func initEditor() {
 
 	editorConfig.screenrows = height
 	editorConfig.screencols = width
+	editorConfig.cursor_x = 0
+	editorConfig.cursor_y = 0
 }
 
 func main() {
